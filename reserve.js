3911d8330173ca39385cb0feef8a1bdbfa04c866
Yuuki Suzuki <07yuki1101@gmail.com>
2026-02-28 11:18:16 +0900

    予約時に電話番号を使って顧客を管理

--- reserve.js
+++ reserve.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyB01-MWSZl4Us-XUyXEfeLQjgxUHEUtpQM",
   authDomain: "training-log-app-d219c.firebaseapp.com",
@@ -19,21 +20,51 @@ form.addEventListener('submit', async (e) => {
 
   const formData = new FormData(form);
 
-  await addDoc(collection(db, 'reservations'), {
-    name: formData.get('name'),
-    dogType: formData.get('dog-type'),
-    menu: formData.get('menu'),
-    date: formData.get('date'),
-    time: formData.get('time'),
+  const name = formData.get('name');
+  const phone = formData.get('phone');
+  const dogType = formData.get('dog-type');
+  const menu = formData.get('menu');
+  const date = formData.get('date');
+  const time = formData.get('time');
+
+
+  try{
+    const q = query(
+      collection(db,'customers'),
+      where('phone', '==',phone)
+    );
+    const snapshot = await getDocs(q);
+    let customerId;
+
+    if(!snapshot.empty){
+      customerId = snapshot.docs[0].id;
+    }else{
+      const newCustomer = await addDoc(collection(db,'customers'),{
+        name,
+        phone,
+        dogType,
+        createdAt:serverTimestamp()
+      });
+      customerId = newCustomer.id;
+    }
+    await addDoc(collection(db, 'reservations'), {
+    customerId,
+    menu,
+    date,
+    time,
     status: '予約中',
     createdAt: serverTimestamp()
   });
   alert('予約が送信されました！')
   form.reset();
+  }catch(error){
+    console.error(error);
+    alert('エラーが発生しました')
+  }
+  
 });
 
 const menuSelect = document.getElementById('menu');
-
 const loadService = async()=>{
   const q = query(
     collection(db,'services'),
@@ -45,7 +76,6 @@ const loadService = async()=>{
     const option = document.createElement('option');
     option.value = doc.id;
     option.textContent = data.name;
-
     menuSelect.appendChild(option);
   });
 };
